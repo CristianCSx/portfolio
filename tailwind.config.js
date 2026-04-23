da47e3834691533e4967525142de84a5c97f3cec
Cristian <cristian18cas@gmail.com>
2026-04-23 08:43:42 +0200

    Onest/Cascadia Code fonts and Flowbite integration

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Onest', 'sans-serif'],
+        mono: ['Cascadia Code', 'monospace'],
+      }
+    }
+  },
+  plugins: []
+}
