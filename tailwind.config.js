484bcc26a6b4fc6c206817df595d701bf1b3f94c
Cristian <cristian18cas@gmail.com>
2026-05-11 17:51:50 +0200

    Add tailwind typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+import typography from '@tailwindcss/typography';
 export default {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,ts,tsx}"
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Onest', 'sans-serif'],
-        mono: ['Cascadia Code', 'monospace'],
-      }
-    }
-  },
-  plugins: []
+	content: [
+		"./src/**/*.{astro,html,js,jsx,ts,tsx}",
+		"./node_modules/flowbite/**/*.js"
+	],
+	theme: {
+		extend: {
+		fontFamily: {
+			sans: ['Onest', 'sans-serif'],
+			mono: ['Cascadia Code', 'monospace'],
+		}
+		}
+	},
+	plugins: [
+		typography
+	]
 }
