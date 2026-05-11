83087d674ba0f544c433b6c698b9c04c0f08d367
Cristian <cristian18cas@gmail.com>
2026-05-12 00:03:06 +0200

    Add intial working translation config

---
+++ src/i18n/utils.ts
@@ -0,0 +1,22 @@
+import es from './es.json';
+import en from './en.json';
+
+export const languages = {
+	es,
+	en
+};
+
+export type Lang = keyof typeof languages;
+
+export function getLangFromUrl(url: URL): Lang {
+	// ✅ FIX: detección robusta por prefijo
+	return url.pathname.startsWith('/en') ? 'en' : 'es';
+}
+
+export function useTranslations(lang: Lang) {
+	return function t(key: string) {
+		return key
+			.split('.')
+			.reduce((obj: any, i) => obj?.[i], languages[lang]) || key;
+	};
+}
